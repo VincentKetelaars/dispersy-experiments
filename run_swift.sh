864e7fc29ad9f52ec5d8160260ae44ff7581b561
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-09-30 17:50:34 +0200

    Added run_swift.sh, to test swift changes directly

---
+++ run_swift.sh
@@ -0,0 +1,5 @@
+libswift/swift -l 127.0.0.1:12345 -f "/home/vincent/Desktop/test_very_large/Coupling - [2x01] - The Man with Two Legs.mkv" &>~/Desktop/logs4 &
+libswift/swift -t 127.0.0.1:12345 -g 0.0.0.0:8080 -w &>~/Desktop/logs5 &
+
+# Hash: 2a7ce792592e2c0aa9f3bb0ce71de80b4cc00c56
+wait
