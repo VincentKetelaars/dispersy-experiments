5c95562f4a87bcba1044c638d79b1eec6b68f83e
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-10-07 08:17:11 +0200

    Libswift update.

--- run_swift_live.sh
+++ run_swift_live.sh
@@ -1,9 +1,11 @@
-swift=tribler/Tribler/SwiftEngine/swift
-# swift=libswift/swift
+# swift=tribler/Tribler/SwiftEngine/swift
+swift=libswift/swift
 
 swift/run_live/source-file.sh ${swift} &
+sleep 1
 swift/run_live/client.sh ${swift} &
+sleep 1
 swift/run_live/player.sh  &
 
-sleep 10
+sleep 15
 pkill swift
