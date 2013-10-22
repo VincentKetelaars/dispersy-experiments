fb022d1201cee79ab05efe5cbf60db87f4bcdd45
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-10-22 13:24:38 +0200

    Gitignore now ignores *.o and multi_interface handles chars and strings better

--- swift/partial/multi_interface.cpp
+++ swift/partial/multi_interface.cpp
@@ -1,10 +1,13 @@
+#include <string>
+#include <string.h>
 #include <sys/socket.h>
 #include <stdio.h>
-#include <string.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <errno.h>
+
+using namespace std;
 
 int bind (sockaddr_in sa) {
 	int fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -53,7 +56,7 @@ sockaddr_in create_sockaddr(int port, char *addr) {
 	return si;
 }
 
-int send(int sock, sockaddr_in peer, const char *content) {
+int send(int sock, sockaddr_in peer, char *content) {
 	return sendto(sock,content,strlen(content),0,(struct sockaddr*)&peer,sizeof(peer));
 }
 
@@ -85,16 +88,17 @@ int send_msg(int sock, sockaddr_in peer, char *content) {
 
 int main(int argc, char *argv[]) {
 	printf("Main\n");
-	struct sockaddr_in sock = create_sockaddr(5555, "193.156.108.78");
+	struct sockaddr_in sock = create_sockaddr(5555, (char *) "193.156.108.78");
 	int fd = bind(sock);
 	if (fd < 0) {
 		perror("Not really a socket..");
 	}
 
-	struct sockaddr_in peer1 = create_sockaddr(55555, "193.156.108.67");
-	struct sockaddr_in peer2 = create_sockaddr(6428, "130.161.211.194");
+	struct sockaddr_in peer1 = create_sockaddr(55555, (char *) "193.156.108.67");
+	struct sockaddr_in peer2 = create_sockaddr(6428, (char *) "130.161.211.194");
 
-	char * msg = "testing";
+	std::string s = "testing";
+	char * msg = (char *) s.c_str();
 	if (send_msg(fd, peer1, msg) < 0) {
 		perror("Peer1 cannot send");
 	}
