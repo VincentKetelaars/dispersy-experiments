b8df87980b58321c9090b53544409ae7218d7a5a
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-10-22 12:06:46 +0200

    Added sendmsg to multi_interface.cpp

--- swift/partial/multi_interface.cpp
+++ swift/partial/multi_interface.cpp
@@ -11,10 +11,10 @@ int bind (sockaddr_in sa) {
 	if (fd < 0) {
 		perror("Creating socket failed");
 	}
-//	const char *devname = "wlan0";
-//	if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, devname, strlen(devname)) < 0) {
-//		perror("Setting option failed");
-//	}
+	const char *devname = "wlan0";
+	if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, devname, strlen(devname)) < 0) { // Needs root permission??
+		perror("Setting option failed");
+	}
 	if (bind(fd, (sockaddr*)&sa, sizeof(sa)) < 0) {
 		perror("Binding failed");
 	}
@@ -22,7 +22,27 @@ int bind (sockaddr_in sa) {
 	return fd;
 }
 
-sockaddr_in create_sockaddr(int port, const char *addr) {
+//int bind_raw (sockaddr_in sa) {
+//	int fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+//	if (fd < 0) {
+//		perror("Creating socket failed");
+//	}
+//	int on = 1;
+//	if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, (char *) &on, sizeof(on)) < 0) {
+//		fprintf(stderr, "Cannot set IP_HDRINCL: %s\n", strerror(errno));
+//	}
+//	const char *devname = "wlan0";
+//	if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, devname, strlen(devname)) < 0) { // Needs root permission??
+//		perror("Setting option failed");
+//	}
+//	if (bind(fd, (sockaddr*)&sa, sizeof(sa)) < 0) {
+//		perror("Binding failed");
+//	}
+//
+//	return fd;
+//}
+
+sockaddr_in create_sockaddr(int port, char *addr) {
 	struct sockaddr_in si;
 	si.sin_family = AF_INET;
 	si.sin_port = htons(port);
@@ -33,8 +53,34 @@ sockaddr_in create_sockaddr(int port, const char *addr) {
 	return si;
 }
 
-int send(int sock, sockaddr_in peer, const char *message) {
-	return sendto(sock,message,sizeof(message),0,(struct sockaddr*)&peer,sizeof(peer));
+int send(int sock, sockaddr_in peer, const char *content) {
+	return sendto(sock,content,strlen(content),0,(struct sockaddr*)&peer,sizeof(peer));
+}
+
+//int send_raw(int sock, sockaddr_in peer, const char *message) {
+//	struct opacket4 op4;
+//	void *packet;
+//	headersize = sizeof(op4.ip) + sizeof(op4.udp);
+//	packetsize = headersize + strlen(message);
+//	op4.ip.ip_len = htons(packetsize);
+//	packet = &op4;
+//	return sendto(sock,packet,strlen(message),0,(struct sockaddr*)&peer,sizeof(peer));
+//}
+
+int send_msg(int sock, sockaddr_in peer, char *content) {
+	struct iovec iov[1];
+	iov[0].iov_base=content;
+	iov[0].iov_len=sizeof(content);
+
+	struct msghdr message;
+	message.msg_name=(struct sockaddr*)&peer;
+	message.msg_namelen=sizeof(peer);
+	message.msg_iov=iov;
+	message.msg_iovlen=1;
+	message.msg_control=0;
+	message.msg_controllen=0;
+
+	return sendmsg(sock,&message,0);
 }
 
 int main(int argc, char *argv[]) {
@@ -48,10 +94,11 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in peer1 = create_sockaddr(55555, "193.156.108.67");
 	struct sockaddr_in peer2 = create_sockaddr(6428, "130.161.211.194");
 
-	if (send(fd, peer1, "testing") < 0) {
+	char * msg = "testing";
+	if (send_msg(fd, peer1, msg) < 0) {
 		perror("Peer1 cannot send");
 	}
-	if (send(fd, peer2, "yeah") < 0) {
+	if (send_msg(fd, peer2, msg) < 0) {
 		perror("Peer2 cannot send");
 	}
 	return 0;
