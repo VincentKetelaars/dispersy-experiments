1950771549a2642b6775afc4b43b0f114b69a266
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-11-27 15:51:51 +0100

    src.main replaces the identical main functions in api and dispersyinstance. run_api.sh calls the api directly, similar to run.sh

---
+++ run_api.sh
@@ -0,0 +1,15 @@
+export PYTHONPATH=${PYTHONPATH}:${PWD}:${PWD}/tribler
+
+python -O -m src.api -d /home/vincent/Desktop/test_large -D /home/vincent/Desktop/tests_dest -t 5 \
+-l 127.0.0.1:12346 -p 127.0.0.1:12345 &> ~/Desktop/logs1 &
+python -O -m src.api -d /home/vincent/Desktop/tests -D /home/vincent/Desktop/tests_dest_2 -t 5 \
+-l 127.0.0.1:12345 -p 127.0.0.1:12346 &> ~/Desktop/logs2 &
+
+#sleep 10
+
+#python -O -m src.dispersy_instance -i -D /home/vincent/Desktop/tests_dest_2 -t 10 -P 12345 11111 -p 12121 &> ~/Desktop/logs3 &
+
+wait
+
+grep --text "\->\|<\-\|CANDIDATES" ~/Desktop/logs1 > ~/Desktop/logs3
+grep --text "\->\|<\-\|CANDIDATES" ~/Desktop/logs2 > ~/Desktop/logs4
--- src/api.py
+++ src/api.py
@@ -14,13 +14,8 @@ from src.definitions import STATE_NOT, STATE_RUNNING, MESSAGE_KEY_ADD_FILE, MESS
 MESSAGE_KEY_ADD_SOCKET, MESSAGE_KEY_INTERFACE_UP, MESSAGE_KEY_MONITOR_DIRECTORY, MESSAGE_KEY_RECEIVE_FILE, \
 MESSAGE_KEY_RECEIVE_MESSAGE, MESSAGE_KEY_STATE, MESSAGE_KEY_STOP, STATE_DONE
 
-from src.definitions import DEST_DIR, SWIFT_BINPATH, TOTAL_RUN_TIME, DISPERSY_WORK_DIR, SQLITE_DATABASE, \
-BLOOM_FILTER_UPDATE, ENABLE_CANDIDATE_WALKER
-
 from dispersy.logger import get_logger
 from src.tools.runner import CallFunctionThread
-from dispersy.dispersy import Dispersy
-import argparse
 logger = get_logger(__name__)
 
 class PipeHandler(object):
@@ -334,66 +329,8 @@ class ReceiverAPI(PipeHandler):
         
         
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Start Dispersy instance')
-    parser.add_argument("-b", "--bloomfilter", help="Send bloom filter every # seconds")
-    parser.add_argument("-d", "--directory", help="List directory of files to send")
-    parser.add_argument("-D", "--destination", help="List directory to put downloads")
-    parser.add_argument("-f", "--files", nargs="+", help="List files to send")
-    parser.add_argument("-l", "--listen", nargs="+", help="List of sockets to listen to (port, ip4, ip6), space separated")
-    parser.add_argument("-p", "--peers", nargs="+", help="List of Dispersy peers(port, ip4, ip6), space separated")
-    parser.add_argument("-q", "--sqlite_database", default=u":memory:", help="SQLite Database directory")
-    parser.add_argument("-s", "--swift", help="Swift binary path")
-    parser.add_argument("-t", "--time", type=float, help="Set runtime")
-    parser.add_argument("-w", "--work_dir", help="Working directory")
-    parser.add_argument("-W", "--walker", action='store_true', help="Enable candidate walker")
-    args = parser.parse_args()
+    from src.main import main
+    main(API)
     
-    if args.time:
-        TOTAL_RUN_TIME = args.time
-        
-    if args.destination:
-        DEST_DIR = args.destination
-    
-    if args.swift:
-        SWIFT_BINPATH = args.swift
-        
-    if args.work_dir:
-        DISPERSY_WORK_DIR = args.work_dir
-        
-    if args.sqlite_database:
-        SQLITE_DATABASE = args.sqlite_database
-        
-    if args.bloomfilter:
-        BLOOM_FILTER_UPDATE = args.bloomfilter
-        
-    if args.walker:
-        ENABLE_CANDIDATE_WALKER = args.walker
-        
-    localip = "127.0.0.1"
-    local_interface = Dispersy._guess_lan_address(Dispersy._get_interface_addresses())
-    if local_interface is not None:
-        localip = local_interface.address
-        
-    listen = []
-    if args.listen:
-        for a in args.listen:
-            addr = Address.unknown(a)
-            if addr.is_wildcard_ip():
-                addr.set_ipv4(localip)
-            listen.append(addr)
-    
-    peers = []
-    if args.peers:
-        for p in args.peers:
-            addr = Address.unknown(p)
-            if addr.is_wildcard_ip():
-                addr.set_ipv4(localip)
-            peers.append(addr)
-        
-    a = API(DEST_DIR, SWIFT_BINPATH, dispersy_work_dir=DISPERSY_WORK_DIR, sqlite_database=SQLITE_DATABASE,
-            swift_work_dir=DEST_DIR, listen=listen, peers=peers, files_directory=args.directory,
-            files=args.files, run_time=TOTAL_RUN_TIME, bloomfilter_update=BLOOM_FILTER_UPDATE,
-            walker=ENABLE_CANDIDATE_WALKER)
-    a.start()
 
     
