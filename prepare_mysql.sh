108a4645009ab98a8766a49a97f15621934f99c3
Vincent Ketelaars <vincentketelaars@gmail.com>
2014-04-08 09:53:23 +0200

    New configuration for delft

--- prepare_mysql.sh
+++ prepare_mysql.sh
@@ -1,2 +1,2 @@
 mysql -u root -p < create_db.sql
-python -m src.database.ConfigTool import uav/defaultConfiguration_vincent.xml default
+python -m src.database.ConfigTool import uav/defaultConfiguration_delft.xml default
